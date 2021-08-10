264519b593512574a67d8ae9d1bb0d237013ba84
Anamul Haque <82591214+Md-Anamul-Haque@users.noreply.github.com>
2021-08-10 23:31:30 +0600

    Add files via upload

---
+++ recognition.script.js
@@ -0,0 +1,43 @@
+var speechrecognition = window.webkitSpeechRecognition
+var recognition = new speechrecognition()
+var textbox = $("textbox")
+var instructions =$("instructions")
+var content =''
+
+recognition.continuous = true
+
+
+//recognition is started
+
+recognition.onstart = function(){
+    instructions.text("Voice Racognition is on")
+}
+
+recognition.onspeechend =function(){
+    instructions.text("No Activity")
+}
+
+recognition.onerror =function(){
+    instructions.text("Tray Again")
+}
+
+
+recognition.onspeechend =function(event){
+    var=current = event.resultIndex;
+    
+    var transcript = event.results[current][0].transcript
+    
+    content +=transcript
+    
+    textbox.val(content)
+}
+$("start-btn").click(function(event){
+    if(content.length){
+        content +=''
+    }
+    recognition.start()
+})
+
+textbox.on('input', function(){
+    content=$(this).val()
+})
